05e6e7429178583e31831d9c5cff5d5304f37dae
Felix Eckhofer <felix@eckhofer.com>
2016-04-16 16:42:31 +0200

    Use updated paths for xenial

--- scripts/paint-it-black.sh
+++ scripts/paint-it-black.sh
@@ -27,13 +27,13 @@ sedfilter() {
 }
 
 sedfilter \
-	/lib/plymouth/themes/ubuntu-logo/ubuntu-logo.grub \
+	/usr/share/plymouth/themes/ubuntu-logo/ubuntu-logo.grub \
 	's/background_color [0-9]+,[0-9]+,[0-9]+/background_color 0,0,0/'
 sedfilter \
-	/lib/plymouth/themes/ubuntu-logo/ubuntu-logo.script \
+	/usr/share/plymouth/themes/ubuntu-logo/ubuntu-logo.script \
 	's/^(Window.SetBackground(Top|Bottom)Color ?)\(0\.[0-9]{1,2}, 0\.[0-9]{1,2}, 0\.[0-9]{1,2}\)/\1(0.00, 0.00, 0.00)/'
 sedfilter \
-	/lib/plymouth/themes/ubuntu-text/ubuntu-text.plymouth \
+	/usr/share/plymouth/themes/ubuntu-text/ubuntu-text.plymouth \
 	's/^black=0x[a-fA-F0-9]{6}/black=0x000000/'
 
 notice "You may want to run 'update-initramfs -u -k all && update-grub' now :)"
